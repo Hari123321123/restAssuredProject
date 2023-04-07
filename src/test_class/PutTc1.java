dbad91ce99b20d63eee7db79009bf2c24c614b35
Hari123321123 <127378093+Hari123321123@users.noreply.github.com>
2023-04-07 19:40:57 +0530

    Update PutTc1.java

--- src/test_class/PutTc1.java
+++ src/test_class/PutTc1.java
@@ -61,8 +61,8 @@ public class PutTc1 {
 		
 		//Extract Requestbody parameters 
 		JsonPath req = new JsonPath(requestbody);
-		String req_name = jsp.getString("name");
-		String req_job = jsp.getString("job");
+		String req_name = req.getString("name");
+		String req_job = req.getString("job");
 
 		// validate responsebody parameter
 		Assert.assertEquals(res_name, req_name);
